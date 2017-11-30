4f0fc14b74396f8a76dd9af3a721158061dae72d
MarcyMARLY <msisengali@gmail.com>
2017-11-30 07:08:43 +0600

    second version

--- app/src/main/java/com/example/caspar/cnrouter/MainActivity.java
+++ app/src/main/java/com/example/caspar/cnrouter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.caspar.cnrouter;
 
 import android.media.Image;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private ImageView router1;
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private Button n22;
     private Button n32;
     private Button n42;
+
     private int ch1=0,ch2=0;
     Router r1;
     Router r2;
@@ -56,13 +59,13 @@ public class MainActivity extends AppCompatActivity {
         r3.addDynRow("171.128.0.94/28", "171.128.0.0/26", 12);
 
 
-        router1 = (ImageView) findViewById(R.id.router1);
-        router2 = (ImageView) findViewById(R.id.router2);
-        router3 = (ImageView) findViewById(R.id.router3);
-        N1C1 = (ImageView) findViewById(R.id.N1C1);
-        N1C2 = (ImageView) findViewById(R.id.N1C2);
-        N2C1 = (ImageView) findViewById(R.id.N2C1);
-        N3C1 = (ImageView) findViewById(R.id.N3C1);
+        router1 = (ImageView) findViewById(R.id.prouter1);
+        router2 = (ImageView) findViewById(R.id.prouter2);
+        router3 = (ImageView) findViewById(R.id.prouter3);
+        N1C1 = (ImageView) findViewById(R.id.pN1C1);
+        N1C2 = (ImageView) findViewById(R.id.pN1C2);
+        N2C1 = (ImageView) findViewById(R.id.pN2C1);
+        N3C1 = (ImageView) findViewById(R.id.pN3C1);
         tv1 = (TextView) findViewById(R.id.tv1);
         n11 = (Button) findViewById(R.id.n11);
         n21 = (Button) findViewById(R.id.n21);
@@ -72,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
         n22 = (Button) findViewById(R.id.n22);
         n32 = (Button) findViewById(R.id.n32);
         n42 = (Button) findViewById(R.id.n42);
+
+
 
         n11.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,30 +149,101 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    public void findDirCon(int ch1,int ch2){
+    public void findDirCon(final int ch1,final int ch2){
         if(ch1==ch2){
             tv1.setText(tv1.getText()+"\n"+"Inside one network the message is successfully delivered." );
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getImageView(convertPosToIP(ch1).getIPaddress()).setVisibility(View.VISIBLE);
+                }
+            }, 1500);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getImageView(convertPosToIP(ch1).getIPaddress()).setVisibility(View.GONE);
+                    getImageView(ch2).setVisibility(View.VISIBLE);
+                }
+            }, 1600);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getImageView(ch2).setVisibility(View.GONE);
+                }
+            }, 1900);
         }else {
             ArrayList<LocalRow> directlyConTable = convertPosToIP(ch1).getRTable();
             for (int i = 0; i < directlyConTable.size(); i++) {
                 if (convertPosToIP(ch2).getIPaddress() == directlyConTable.get(i).getNetworkAddress()) {
                     // t=true;
                     tv1.setText(tv1.getText()+"\n"+"Network " + ch1 + " is directly connected  to Network " + ch2 + ". The message is successfully delivered.");
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            getImageView(ch1).setVisibility(View.VISIBLE);
+                        }
+                    }, 400);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            getImageView(ch1).setVisibility(View.GONE);
+                            getImageView(convertPosToIP(ch1).getIPaddress()).setVisibility(View.VISIBLE);
+                        }
+                    }, 500);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            getImageView(convertPosToIP(ch1).getIPaddress()).setVisibility(View.GONE);
+                            getImageView(ch2).setVisibility(View.VISIBLE);
+                        }
+                    }, 900);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            getImageView(ch2).setVisibility(View.GONE);
+                        }
+                    }, 1200);
                 }
             }
         }
     }
-    public void findRemCon(int ch1,int ch2){
+    public void findRemCon(final int ch1,final int ch2){
         ArrayList<RemoteRow> remoteConTable = convertPosToIP(ch1).getRRTable();
         for (int i = 0; i < remoteConTable.size(); i++) {
             if (convertPosToIP(ch2).getIPaddress() == remoteConTable.get(i).getNetworkAddress()) {
                 tv1.setText(tv1.getText() + "There's a path: Next hop: " + convertIPtoRouter(remoteConTable.get(i).getNextHop()) + " Metrics: " + remoteConTable.get(i).getDistance() + "\n");
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getImageView(convertPosToIP(ch1).getIPaddress()).setVisibility(View.VISIBLE);
+                    }
+                }, 900);
+
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getImageView(convertPosToIP(ch1).getIPaddress()).setVisibility(View.GONE);
+                        getImageView(convertPosToIP(ch2).getIPaddress()).setVisibility(View.VISIBLE);
+                    }
+                }, 1000);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getImageView(convertPosToIP(ch2).getIPaddress()).setVisibility(View.GONE);
+                    }
+                }, 1300);
                 findDirCon(ch2,ch2);
             }
         }
     }
 
-    public void findThePath(int ch1,int ch2)
+    public void findThePath(final int ch1,final int ch2)
     {
         //boolean t = false;
         tv1.setText("");
@@ -177,6 +253,31 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < remoteConTable.size(); i++) {
                 if (convertPosToIP(ch2).getIPaddress() == remoteConTable.get(i).getNetworkAddress()) {
                     tv1.setText(tv1.getText() + "There's a path: Next hop: " + convertIPtoRouter(remoteConTable.get(i).getNextHop()) + " Metrics: " + remoteConTable.get(i).getDistance() + "\n");
+                    final String ip = remoteConTable.get(i).getNextHop();
+                    getImageView(ch1).setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            getImageView(ch1).setVisibility(View.GONE);
+                            getImageView(convertPosToIP(ch1).getIPaddress()).setVisibility(View.VISIBLE);
+                        }
+                    }, 100);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            getImageView(convertPosToIP(ch1).getIPaddress()).setVisibility(View.GONE);
+                           // getImageView(ip).setVisibility(View.VISIBLE);
+                        }
+                    }, 400);
+                    /*new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            getImageView(ip).setVisibility(View.GONE);
+                        }
+                    }, 700);*/
                     if(remoteConTable.get(i).getNextHop() == "171.128.0.0/26"){
                         if(remoteConTable.get(i).getNetworkAddress()==remoteConTable.get(i).getNextHop()){
                             findDirCon(convertIPToPos(remoteConTable.get(i).getNextHop()),convertIPToPos(remoteConTable.get(i).getNetworkAddress()));
@@ -230,6 +331,30 @@ public class MainActivity extends AppCompatActivity {
             return "r2";
         }
         return "r3";
+    }
+    public ImageView getImageView(int ch){
+        if(ch==1){
+            return N1C1;
+        }
+        if(ch==2){
+            return N1C2;
+        }
+        if(ch==3){
+            return N2C1;
+        }
+        else{
+            return N3C1;
+        }
+    }
+    public ImageView getImageView(String ip){
+        if(ip == "171.128.0.0/26" ||ip == "171.128.0.0/26" ){
+            return router1;
+        }
+        if(ip == "171.128.0.94/28"){
+            return router2;
+        }
+        return router3;
+
     }
 
 }
